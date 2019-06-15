8fc22c1dc074608bd4b523527db5d53cc54767a1
outsiderem <reemharbi.3@gmail.com>
2019-06-16 02:54:30 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else {
-		alert("Sorry, try again.");
-	}
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+function flipCard(cardID) {
+cardsInPlay.push(cards[cardID]);
+console.log("User flipped " + cards[cardID]);
+
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
